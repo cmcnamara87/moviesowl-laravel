91ceabc1c3b9f213e0f34390f9230fda372ad78f
Kelvin Tamzil <kelvintamzil@yahoo.com>
2015-09-20 14:02:32 +1000

    add: imdb id col

---
+++ database/migrations/2015_09_20_132047_add_imdbid_to_movies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddImdbidToMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function(Blueprint $table)
+        {
+            $table->integer('imdb_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movies', function(Blueprint $table)
+        {
+            $table->dropColumn('imdb_id');
+        });
+    }
+}
