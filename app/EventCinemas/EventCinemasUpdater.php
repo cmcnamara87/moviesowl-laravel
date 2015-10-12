56663f03c33ca20907fb1a61421bccb00123c3a5
Craig McNamara <cmcnamara87@gmail.com>
2015-10-13 00:24:47 +1000

    feat(movies importer): integrate google movies

--- app/EventCinemas/EventCinemasUpdater.php
+++ app/EventCinemas/EventCinemasUpdater.php
@@ -32,8 +32,6 @@ class EventCinemasUpdater {
                          OMDBApi $omdbApi, ConsoleOutput $output)
     {
         $this->eventCinemasApi = $eventCinemasApi;
-        $this->rottenTomatoesApi = $rottenTomatoesApi;
-        $this->omdbApi = $omdbApi;
         $this->output = $output;
     }
 
@@ -70,124 +68,10 @@ class EventCinemasUpdater {
      */
     public function getOrCreateMovie($eventMovie)
     {
-        $movie = Movie::firstOrCreate(array('title' => $eventMovie->title));
-
-        Log::info("- " . $movie->title);
-
-        if($this->hasRecentMovieInfo($movie)) {
-            Log::info("-- Up to date");
-            return $movie;
-        }
-
-        Log::info("-- Needs update " . $movie->updated_at->toDateTimeString() . ' ' . Carbon::today()->toDateTimeString());
-        
-        if($movie->rotten_tomatoes_id) {
-            $rtMovie = $this->rottenTomatoesApi->getMovieById($movie->rotten_tomatoes_id);
-        } else {
-            $rtMovie = $this->rottenTomatoesApi->getMovie($eventMovie->title);
-        }
-
-        if(!$rtMovie) {
-            return null;
-        }
-        Log::info("-- Rotten Tomatoes Match " . $rtMovie->title);
-
-        $movie->tomato_meter = $rtMovie->ratings->critics_score;
-        $movie->rotten_tomatoes_id = $rtMovie->id;
-        if (!$movie->imdb_id) {
-            $movie->imdb_id = $this->getImdbIdFromRtmovie($rtMovie);
-        }
-        $movie->save();
-
-
-        if (isset($rtMovie->abridged_directors)) {
-            $abridged_directors = $rtMovie->abridged_directors;
-        }
-        else {
-            $abridged_directors = [];       
-        }
-
-        $movieDetails = MovieDetails::firstOrCreate(array('movie_id' => $movie->id));
-        $movieDetails->fill([
-            "title" => $rtMovie->title,
-            "synopsis" => $rtMovie->synopsis,
-            "run_time" => $rtMovie->runtime,
-            "director" => array_reduce($abridged_directors, function($carry, $directors) {
-                if(strlen($carry)) {
-                    $carry .= ', ';
-                }
-                return $carry . $directors->name;
-            }, ""),
-            "cast" => array_reduce($rtMovie->abridged_cast, function($carry, $castMember) {
-                if(strlen($carry)) {
-                    $carry .= ', ';
-                }
-                return $carry . $castMember->name;
-            }, ""),
-            "poster" => $this->getHiResPosterUrl($movie->imdb_id),
-            "tomato_meter" => $rtMovie->ratings->critics_score,
-            "genre" => array_reduce($rtMovie->genres, function($carry, $genres) {
-                if(strlen($carry)) {
-                    $carry .= ', ';
-                }
-                return $carry . $genres;
-            }, ""),
-        ]);
-
-        $movieDetails->save();
-        // Need to touch in case there was no change in data
-        $movieDetails->touch();
-
-        return $movie;
+        return Movie::firstOrCreate(array('title' => $eventMovie->title));
     }
 
-    public function getImdbIdFromRtmovie ($rtMovie) {
-        if (!isset($rtMovie->alternate_ids)) {
-            return null;
-        }
-        if (!isset($rtMovie->alternate_ids->imdb)) {
-            return null;
-        }
-        return $rtMovie->alternate_ids->imdb;
-    }
 
-    /**
-    get high resolution poster
-    Change poster url from rotten tomatoes to get higher resolution poster
-    **/
-    public function getHiResPosterUrl ($imdbId) {
-        if (!$imdbId) {
-            return "images/no_poster.jpg";
-        }
-        $asset = "images/posters/" . $imdbId . ".jpg";
-        $posterPath = public_path() . "/" . $asset;
-        if(!file_exists($posterPath)) {
-            $posterUrl = $this->getPosterUrl($imdbId);
-            if (!$posterUrl) {
-                return "images/no_poster.jpg";
-            }
-            try {
-                $img = Image::make($posterUrl);
-                $img->save($posterPath);
-                Log::info("---  Saved poster");
-            } catch (Exception $e) {
-                return "images/no_poster.jpg";
-            }
-        }
-        return $asset;
-    }
-
-    public function getPosterUrl($imdbId) {
-        $omdbMovie = $this->omdbApi->getMovieByImdbId("tt" . $imdbId);
-        if (!isset($omdbMovie->Poster)) {
-            return null;
-        }
-        $posterUrl = str_replace("SX300", "SX700", $omdbMovie->Poster);
-        if ($posterUrl == "N/A") {
-            return null;
-        }
-        return $posterUrl;
-    }
 
     /**
      * @param EventCinemasSession $session
@@ -229,36 +113,5 @@ class EventCinemasUpdater {
                 $this->getOrCreateShowing($session, $cinema, $movie);
             }
         }
-    }
-
-    /**
-     * @param $movie
-     * @return mixed
-     */
-    public function hasRecentMovieInfo($movie)
-    {
-        // Is it new?
-        if (!$movie->details) {
-            return false;
-        }
-        return $this->wasUpdatedToday($movie->details);
-    }
-
-    /**
-     * @param $movie
-     * @return bool
-     */
-    public function isJustCreated($movie)
-    {
-        return $movie->updated_at->eq($movie->created_at);
-    }
-
-    /**
-     * @param $movie
-     * @return mixed
-     */
-    public function wasUpdatedToday($movieDetails)
-    {
-        return $movieDetails->updated_at->gte(Carbon::today());
     }
 }
