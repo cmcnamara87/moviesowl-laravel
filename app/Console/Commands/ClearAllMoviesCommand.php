611882ebfe47038fef2246723aa54b6b5cf9f945
Craig McNamara <cmcnamara87@gmail.com>
2015-12-31 11:05:10 +1000

    refactor(clearing): add options to clearing sessions (for today or tomorrow, no longer clears all sessions)

--- app/Console/Commands/ClearAllMoviesCommand.php
+++ app/Console/Commands/ClearAllMoviesCommand.php
@@ -2,8 +2,10 @@
 
 namespace MoviesOwl\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use MoviesOwl\Showings\Showing;
 
 class ClearAllMoviesCommand extends Command
 {
@@ -40,8 +42,35 @@ class ClearAllMoviesCommand extends Command
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-//        DB::table('cinemas')->truncate();
-        DB::table('showings')->truncate();
+        $this->info('1. Today');
+        $this->info('2. Tomorrow');
+        $this->info('3. Both');
+        $dayToClear = $this->ask('Clear', false);
+
+        if(!$dayToClear) {
+            $this->info('Please specify a day');
+            return;
+        }
+        if($dayToClear == '1') {
+            $startingAfter = Carbon::today();
+            $endOfDay = $startingAfter->copy()->endOfDay();
+        }
+        if($dayToClear == '2') {
+            $startingAfter = Carbon::tomorrow();
+            $endOfDay = $startingAfter->copy()->endOfDay();
+        }
+        if($dayToClear == '3') {
+            $startingAfter = Carbon::today();
+            $endOfDay = $startingAfter->copy()->tomorrow()->endOfDay();
+        }
+
+        $showings = Showing::where('start_time', '>=', $startingAfter->toDateTimeString())
+            ->where('start_time', '<=', $endOfDay->toDateTimeString())
+            ->get();
+
+        foreach($showings as $showing) {
+            $showing->delete();
+        }
 
         // supposed to only apply to a single connection and reset it's self
         // but I like to explicitly undo what I've done for clarity
