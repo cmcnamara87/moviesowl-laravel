ce495750b1df91b590ee3817af0caf6047b92325
Craig McNamara <cmcnamara87@gmail.com>
2016-01-02 22:31:01 +1000

    refactor(showing): change styling of showing card

--- resources/views/includes/showing.blade.php
+++ resources/views/includes/showing.blade.php
@@ -1,4 +1,4 @@
-<a class="time" href="{{ URL::to('showings/'. $showing->id) }}">
+<a class="time" href="{{ URL::to('showings/'. $showing->id . '?starting_after=' . $startingAfter->timestamp) }}">
                             <span class="time__time">
                                 {{ $showing->start_time->format('h:i A') }}
                             </span>
@@ -8,15 +8,17 @@
                                 {{ $showing->screen_type }}
                             </span>
     @endif
-    @if($showing->showing_type != "standard")
+    @if($showing->showing_type != "standard" && $showing->showing_type != "")
         <span class="time__3d">
                                 {{ $showing->showing_type }}
                             </span>
     @endif
 
+    @if($showing->cinema_size && $showing->screen_type == 'standard')
     <span class="time__type">
-                                {{ $showing->cinema_size }} Cinema
-                            </span>
+        {{ $showing->cinema_size }} Cinema
+    </span>
+    @endif
 
     <div class="progress">
         <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
