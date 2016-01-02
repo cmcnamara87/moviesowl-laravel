b047891d8a40f0108dd7a9bb4b07672e0c9a9268
Craig McNamara <cmcnamara87@gmail.com>
2016-01-03 00:42:25 +1000

    refactor( imdv ): improve command

--- app/Console/Commands/AddMissingImdbIdsCommand.php
+++ app/Console/Commands/AddMissingImdbIdsCommand.php
@@ -78,8 +78,9 @@ class AddMissingImdbIdsCommand extends Command
 
         $movies = array_reduce($movieIds, function ($carry, $movieId) {
             $movie = Movie::find($movieId);
-            if ($movie->rotten_tomatoes_id == '' || $movie->imdb_id == '') {
-                $this->info($movie->title);
+            $this->info($movie->title . ' ' . $movie->rotten_tomatoes_id . ' ' . $movie->imdb_id);
+            if ($movie->rotten_tomatoes_id == '' || $movie->rotten_tomatoes_id == 0 || $movie->imdb_id == '') {
+                $this->info('- Missing ' . $movie->title);
                 $carry[] = $movie;
             }
             return $carry;
