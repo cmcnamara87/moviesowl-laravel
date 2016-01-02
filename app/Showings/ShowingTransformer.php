1ebfd8ae2f5d06946d6f9c981097cd6ae4665e7d
Craig McNamara <cmcnamara87@gmail.com>
2016-01-03 00:12:44 +1000

    feat(showings api): add fullness percentage to response

--- app/Showings/ShowingTransformer.php
+++ app/Showings/ShowingTransformer.php
@@ -16,6 +16,15 @@ class ShowingTransformer extends TransformerAbstract {
 
 
     public function transform(Showing $showing) {
+        $availableCount = 0;
+        foreach($showing->seats as $row) {
+            foreach($row as $seat) {
+                if($seat == 'available') {
+                    $availableCount += 1;
+                }
+            }
+        }
+
         return [
             'id'                => (int)$showing->id,
             'start_time'        => $showing->start_time->timestamp,
@@ -26,6 +35,7 @@ class ShowingTransformer extends TransformerAbstract {
             "cinema_size"       => $showing->cinema_size,
             "seats"             => $showing->seats,
             "seats_count"       => $showing->seats_count,
+            "full"              => floor($availableCount / $showing->seats_count * 100),
             "seats_updated_at"  => $showing->seats_updated_at->timestamp,
             "event_session_id"  => $showing->event_session_id
         ];
