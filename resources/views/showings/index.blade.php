f63628c0dbc465566d613edf0d7fb08a5aef22e9
Craig McNamara <cmcnamara87@gmail.com>
2016-01-03 11:03:58 +1000

    refactor(showings): add message when no sessions for time of day

--- resources/views/showings/index.blade.php
+++ resources/views/showings/index.blade.php
@@ -23,7 +23,10 @@
                 {{ $movie->synopsis }}
 
                 @foreach ($showingsByTime as $timeOfDay => $showings)
-                    <h4 class="text-uppercase text-muted" style="font-size:14px;margin-bottom: 24px;">{{ $timeOfDay }}</h4>
+                    <h4 class="text-uppercase text-muted" style="font-size:14px;margin-bottom: 16px;">{{ $timeOfDay }}</h4>
+                    @if (!count($showings))
+                        <p class="text-muted" style="margin-bottom: 24px;">No sessions</p>
+                    @endif
                     @foreach (array_chunk($showings, 2) as $showingsRow)
                         <div class="row">
                             @foreach ($showingsRow as $showing)
