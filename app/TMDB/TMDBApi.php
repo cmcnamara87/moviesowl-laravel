3ba3ca4838cc0d938aa70e804ea10e24c0cc8c87
Kelvin Tamzil <kelvintamzil@yahoo.com>
2016-01-21 12:39:15 +1000

    add(class): TMDB Api

--- app/TMDB/TMDBApi.php
+++ app/TMDB/TMDBApi.php
@@ -6,7 +6,14 @@
  * Date: 21/01/2016
  * Time: 12:34 PM
  */
+
+namespace MoviesOwl\TMDB;
+
+
 class TMDBApi
 {
-
+    public function getMovieByImdbId($id) {
+        $url = "http://api.themoviedb.org/3/movie/{$id}?api_key=05629307398e2d06e10c5f0b99fd7b38";
+        return json_decode(@file_get_contents($url));
+    }
 }
