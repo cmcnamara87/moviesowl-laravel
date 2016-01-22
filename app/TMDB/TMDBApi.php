d93deba862aa08d0bee7f7c3835f6b994ad96387
Kelvin Tamzil <kelvintamzil@yahoo.com>
2016-01-22 14:44:32 +1000

    feat(tmdbApi): search tmdb movie by title

--- app/TMDB/TMDBApi.php
+++ app/TMDB/TMDBApi.php
@@ -9,9 +9,10 @@
 
 namespace MoviesOwl\TMDB;
 
-
+use Illuminate\Support\Facades\Log;
 class TMDBApi
 {
+
     public function getMovieByImdbId($id) {
         $url = "http://api.themoviedb.org/3/movie/{$id}?api_key=05629307398e2d06e10c5f0b99fd7b38";
         return json_decode(@file_get_contents($url));
@@ -19,6 +20,11 @@ class TMDBApi
 
     public function  getMovieTrailerByImdbId($id) {
         $url = "http://api.themoviedb.org/3/movie/{$id}/videos?&api_key=05629307398e2d06e10c5f0b99fd7b38";
+        return json_decode(@file_get_contents($url));
+    }
+
+    public function searchTmdbMovieByTitle($title){
+        $url = "http://api.themoviedb.org/3/search/movie?query={$title}&api_key=05629307398e2d06e10c5f0b99fd7b38";
         return json_decode(@file_get_contents($url));
     }
 }
