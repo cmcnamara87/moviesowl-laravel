75486a92d314f3e8b76f2e731f086cf45ec7354b
Craig McNamara <cmcnamara87@gmail.com>
2016-01-28 08:21:20 +1000

    change position off app banner
    I've moved the app banner to the bottom of the screen, above the footer

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,20 +4,20 @@
     @include('includes.head')
 </head>
 <body>
-<div class="app-banner">
-    <h3>Get the MoviesOwl App</h3>
-    <p>Movies Times, Reviews and Tickets in your Pocket</p>
-    <p style="margin-bottom: 0;">
-        <a target="_blank" href="https://itunes.apple.com/au/app/moviesowl-find-great-movies/id1032668935?mt=8"><img src="{{ URL::asset('images/appStore_logo.png') }}" alt=""/></a>
-        <a target="_blank" href="https://play.google.com/store/apps/details?id=com.moviesowl&hl=en"><img src="{{ URL::asset('images/playstore.png') }}" alt=""/></a>
-    </p>
-</div>
 <header>
     @include('includes.header')
 </header>
 <div>
     <div id="main">
         @yield('content')
+    </div>
+    <div class="app-banner">
+        <h3>Get the MoviesOwl App</h3>
+        <p>Movies Times, Reviews and Tickets in your Pocket</p>
+        <p style="margin-bottom: 0;">
+            <a target="_blank" href="https://itunes.apple.com/au/app/moviesowl-find-great-movies/id1032668935?mt=8"><img src="{{ URL::asset('images/appStore_logo.png') }}" alt=""/></a>
+            <a target="_blank" href="https://play.google.com/store/apps/details?id=com.moviesowl&hl=en"><img src="{{ URL::asset('images/playstore.png') }}" alt=""/></a>
+        </p>
     </div>
     <footer>
         @include('includes.footer')
