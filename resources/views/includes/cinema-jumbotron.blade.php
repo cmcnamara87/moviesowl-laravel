f9d937bc415229a949ad81edf72fec98f87e693e
Craig McNamara <cmcnamara87@gmail.com>
2016-01-29 19:24:57 +1000

    fix cinema page, show 'tomorrow' from 1pm

--- resources/views/includes/cinema-jumbotron.blade.php
+++ resources/views/includes/cinema-jumbotron.blade.php
@@ -3,10 +3,11 @@
         <h1>{{ $cinema->location }}</h1>
         <p>Movie Times, Reviews and Tickets for {{ \Carbon\Carbon::$day()->format('l jS \\of F Y') }} - {{ $cinema->city }}, {{ $cinema->country }} Cinema</p>
 
+        {{ \Carbon\Carbon::now()->hour }}
         <div class="btn-group" role="group" aria-label="..." style="margin: 10px 0;">
             <a href="{{ url("{$cinema->slug}/today") }}" class="btn btn-default time-button @if($day == 'today') active @endif">Today</a>
             <a href="{{ url("{$cinema->slug}/now") }}" class="btn btn-default time-button @if($day == 'now') active @endif">Now</a>
-            @if(\Carbon\Carbon::now()->hour > 13)
+            @if(\Carbon\Carbon::now()->hour > 12)
             <a href="{{ url("{$cinema->slug}/tomorrow") }}" class="btn btn-default time-button @if($day == 'tomorrow') active @endif">Tomorrow</a>
             @endif
         </div>
