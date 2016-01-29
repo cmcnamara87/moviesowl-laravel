798a8f114c7322a3deee024083b116a32248f5b8
Craig McNamara <cmcnamara87@gmail.com>
2016-01-29 19:26:18 +1000

    fix(cinema): remove accidental hour

--- resources/views/includes/cinema-jumbotron.blade.php
+++ resources/views/includes/cinema-jumbotron.blade.php
@@ -2,8 +2,7 @@
     <div class="container">
         <h1>{{ $cinema->location }}</h1>
         <p>Movie Times, Reviews and Tickets for {{ \Carbon\Carbon::$day()->format('l jS \\of F Y') }} - {{ $cinema->city }}, {{ $cinema->country }} Cinema</p>
-
-        {{ \Carbon\Carbon::now()->hour }}
+        
         <div class="btn-group" role="group" aria-label="..." style="margin: 10px 0;">
             <a href="{{ url("{$cinema->slug}/today") }}" class="btn btn-default time-button @if($day == 'today') active @endif">Today</a>
             <a href="{{ url("{$cinema->slug}/now") }}" class="btn btn-default time-button @if($day == 'now') active @endif">Now</a>
