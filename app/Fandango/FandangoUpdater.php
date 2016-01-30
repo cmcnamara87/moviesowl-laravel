1ae39d28dd34c9b681b46c555bb43f3f05b62504
Craig McNamara <cmcnamara87@gmail.com>
2016-01-30 12:58:48 +1000

    add fandango class

---
+++ app/Fandango/FandangoUpdater.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: craig
+ * Date: 30/01/16
+ * Time: 12:23 PM
+ */
+
+namespace MoviesOwl\Fandango;
+
+
+class FandangoUpdater {
+    public function update() {
+
+        // boston lat lon
+        // lat: 42.359968
+        // lon: -71.060093
+        // http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-01-30&numDays=2&lat=42.359968&lng=-71.060093&radius=20&units=km&api_key=
+
+        dd('hi');
+    }
+}
