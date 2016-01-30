a8ba2f72c663403b857ed3881444312e925ff193
Kelvin Tamzil <kelvintamzil@gmail.com>
2016-01-30 12:58:48 +1000

    feat(loaders): load data from fandango for boston cinemas

--- app/Console/Commands/LoadMoviesCommand.php
+++ app/Console/Commands/LoadMoviesCommand.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Log;
 use Intervention\Image\ImageManagerStatic as Image;
 use MoviesOwl\Cinemas\Cinema;
 use MoviesOwl\EventCinemas\EventCinemasUpdater;
+use MoviesOwl\Fandango\FandangoUpdater;
 use MoviesOwl\GoogleMovies\GoogleMoviesUpdater;
 use MoviesOwl\Movies\Movie;
 use MoviesOwl\Movies\MovieDetails;
@@ -27,6 +28,7 @@ class LoadMoviesCommand extends Command
     private $eventCinemasUpdater;
     private $googleMoviesUpdater;
     private $movieDetailsUpdater;
+    private $fandangoUpdater;
 
     /**
      * The name and signature of the console command.
@@ -49,12 +51,15 @@ class LoadMoviesCommand extends Command
      */
     public function __construct(EventCinemasUpdater $eventCinemasUpdater,
                                 GoogleMoviesUpdater $googleMoviesUpdater,
-                                MovieDetailsUpdater $movieDetailsUpdater)
+                                MovieDetailsUpdater $movieDetailsUpdater,
+                                FandangoUpdater $fandangoUpdater)
     {
         parent::__construct();
         $this->eventCinemasUpdater = $eventCinemasUpdater;
         $this->googleMoviesUpdater = $googleMoviesUpdater;
         $this->movieDetailsUpdater = $movieDetailsUpdater;
+        $this->fandangoUpdater = $fandangoUpdater;
+
     }
 
     /**
@@ -80,6 +85,7 @@ class LoadMoviesCommand extends Command
 
         $this->eventCinemasUpdater->update();
         $this->googleMoviesUpdater->update();
+        $this->fandangoUpdater->update();
         $this->movieDetailsUpdater->updateAll();
     }
 }
