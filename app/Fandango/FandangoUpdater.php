4afc93a7f62fc58cf1f67bbe1b8636fdd9e65820
Craig McNamara <cmcnamara87@gmail.com>
2016-02-13 18:59:00 +1000

    feat(fandango): add 10 cities to american movies owl

--- app/Fandango/FandangoUpdater.php
+++ app/Fandango/FandangoUpdater.php
@@ -23,25 +23,47 @@ class FandangoUpdater {
         // lon: -71.060093
         // http://data.tmsapi.com/v1.1/movies/showings?startDate=2016-01-30&numDays=2&lat=42.359968&lng=-71.060093&radius=20&units=km&api_key=
         Log::info('Updating Fandango');
-        $this->getMovies();
+
+        // boston mas
+        $this->getMovies('Boston', 'America/New_York', 42.359968, -71.060093);
+        // new york, new york
+        $this->getMovies('New York', 'America/New_York', 40.6643, -73.9385);
+        // LA, california
+        $this->getMovies('Los Angeles', 'America/Los_Angeles', 34.0194, -118.4108);
+        // chicago
+        $this->getMovies('Chicago', 'America/Chicago', 41.8376 ,-87.6818);
+        // houston texas
+        $this->getMovies('Houston', 'America/Chicago', 29.7805, -95.3863);
+        // Philadelphia
+        $this->getMovies('Philadelphia', 'America/New_York', 40.0094, -75.1333);
+        // phoenix arizona
+        $this->getMovies('Phoenix', 'America/Denver', 33.5722, -112.0880);
+        // san antonio texas
+        $this->getMovies('San Antonio', 'America/Chicago', 29.4724, -98.5251);
+        // san diego
+        $this->getMovies('San Diego', 'America/Los_Angeles', 32.8153, -117.1350);
+        // dallas texas
+        $this->getMovies('Dallas', 'America/Chicago', 32.7757, -96.7967);
+        // san jose california
+        $this->getMovies('San Jose', 'America/Los_Angeles', 37.2969, -121.8193);
     }
 
-    public function getMovies() {
+    public function getMovies($cityName, $timezone, $lat, $lon) {
         Log::info('Get sessions');
 
         // Hard coded for boston
-        $tomorrow = Carbon::tomorrow('America/New_York');
+        $tomorrow = Carbon::tomorrow($timezone);
 
-        $url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" . $tomorrow->toDateString() . "&numDays=1&lat=42.359968&lng=-71.060093&radius=20&units=km&api_key=" . env('FANDANGO_API_KEY');
+        $url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=" . $tomorrow->toDateString() . "&numDays=1&lat=$lat&lng=$lon&radius=20&units=km&api_key=" . env('FANDANGO_API_KEY');
         $result = json_decode(@file_get_contents($url));
         foreach ($result as $movieElement) {
             Log::info('Get sessions for '.$movieElement->title);
-            $this->getSessions($movieElement);
+            $this->getSessions($cityName, $timezone, $movieElement);
         }
 
     }
 
-    public function getSessions($movieElement) {
+    public function getSessions($cityName, $timezone, $movieElement) {
         $now = Carbon::now()->toDateTimeString();
         $movie = Movie::firstOrCreate([
             'title' => $movieElement->title
@@ -52,8 +74,8 @@ class FandangoUpdater {
             Log::info('Found Cinema '.$session->theatre->name);
             $cinema = Cinema::firstOrCreate([
                 'location' => $session->theatre->name,
-                'timezone' => 'America/New_York',
-                'city' => 'Boston',
+                'timezone' => $timezone,
+                'city' => $cityName,
                 'country' => 'United States'
             ]);
 
