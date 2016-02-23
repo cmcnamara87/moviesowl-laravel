73c61172a00c4cad8146a07ecb6307a1f8f53b15
Craig McNamara <cmcnamara87@gmail.com>
2016-02-24 08:13:56 +1000

    fix(404): add custom 404 page that points back to the main page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.default')
+@section('title', "Whoops - Movie Times, Reviews and Tickets")
+@section('description', "Find Show times and Buy Tickets")
+@section('content')
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Let's Get Started!</h1>
+            <p class="hidden-xs">Click below to select your Cinema</p>
+            <p>
+                <a class="btn btn-primary btn-large" href="/">
+                    Select Cinema
+                </a>
+            </p>
+        </div>
+    </div>
+    
+@stop
