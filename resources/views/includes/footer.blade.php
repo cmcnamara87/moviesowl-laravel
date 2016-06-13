ac4c67de99f273526557f4ee0c1d43b963731014
Craig McNamara <cmcnamara87@gmail.com>
2016-06-13 11:32:14 +1000

    💄(footer): remove name

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <div style="background-color: #eee;padding:40px 0;font-size:12px;">
     <div class="container">
-        <p class="text-muted">MoviesOwl - Copyright Kelvin Tamzil 2016.<br/>MoviesOwl claims no copyright over the images, descriptions or session times displayed on this website.</p>
+        <p class="text-muted">MoviesOwl claims no copyright over the images, descriptions or session times displayed on this website.</p>
     </div>
 </div>
 
