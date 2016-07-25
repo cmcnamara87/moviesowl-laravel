630a2979f95461bec08719ddd2e231b95eba7a01
Craig McNamara <cmcnamara87@gmail.com>
2016-07-25 16:14:57 +1000

    feat(analytics): add get site control

--- resources/views/includes/analyticstracking.blade.php
+++ resources/views/includes/analyticstracking.blade.php
@@ -1,4 +1,9 @@
 <script>
+    (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+        t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+    })(window, document, '_gscq','script','//widgets.getsitecontrol.com/49992/script.js');
+</script>
+<script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
