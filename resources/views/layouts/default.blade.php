dd062b09b4e2ca84cf09f1d251d74bcdf00816e3
Craig McNamara <cmcnamara87@gmail.com>
2016-09-07 22:11:04 +1000

    chore: remove app banner since they are no longer in the app store

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -29,14 +29,14 @@
         <!-- /Ad -->
     </div>
     
-    <div class="app-banner" style="margin-top:50px;">
-        <h3>Get the MoviesOwl App</h3>
-        <p>Movies Times, Reviews and Tickets in your Pocket</p>
-        <p style="margin-bottom: 0;">
-            <a target="_blank" href="https://itunes.apple.com/au/app/moviesowl-find-great-movies/id1032668935?mt=8"><img src="{{ URL::asset('images/appStore_logo.png') }}" alt=""/></a>
-            <a target="_blank" href="https://play.google.com/store/apps/details?id=com.moviesowl&hl=en"><img src="{{ URL::asset('images/playstore.png') }}" alt=""/></a>
-        </p>
-    </div>
+    {{--<div class="app-banner" style="margin-top:50px;">--}}
+        {{--<h3>Get the MoviesOwl App</h3>--}}
+        {{--<p>Movies Times, Reviews and Tickets in your Pocket</p>--}}
+        {{--<p style="margin-bottom: 0;">--}}
+            {{--<a target="_blank" href="https://itunes.apple.com/au/app/moviesowl-find-great-movies/id1032668935?mt=8"><img src="{{ URL::asset('images/appStore_logo.png') }}" alt=""/></a>--}}
+            {{--<a target="_blank" href="https://play.google.com/store/apps/details?id=com.moviesowl&hl=en"><img src="{{ URL::asset('images/playstore.png') }}" alt=""/></a>--}}
+        {{--</p>--}}
+    {{--</div>--}}
     <footer>
         @include('includes.footer')
     </footer>
